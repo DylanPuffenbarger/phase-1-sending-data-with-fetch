ae90ad8e53a5f707189bc6246d0452f841986d68
Dylan Puffenbarger <dylanpuffenbarger@gmail.com>
2023-10-01 14:44:03 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+const displayText = (text) => {
+    document.body.innerHTML += `\n<p>${text}</p>`
+}
+
+function submitData(userName, userEmail){
+    return fetch(
+        `http://localhost:3000/users`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                'name': userName,
+                'email': userEmail
+            })
+        }
+    )
+    .then((resp) => resp.json())
+    .then((json) => displayText(json.id))
+    .catch((error) => displayText(error.message));
+}
